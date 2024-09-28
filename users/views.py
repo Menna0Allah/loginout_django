21201d6ca12b05104c27afe5e2f6d226800e3e03
menna <menna.allah04@gmail.com>
2024-09-28 05:03:03 +0300

    initial commit

---
+++ users/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render,redirect,HttpResponse
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.models import User
+from django.contrib import messages
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, "wronge username or password dude but if you do not have an account, create one ;)")
+            return render(request, 'login.html')  
+        
+    return render(request, 'login.html')
+
+   
+    
+def register_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        try:
+            user = User.objects.create_user(username=username, password=password)
+            login(request, user)
+            return redirect('home')
+        except:
+            messages.error(request, 'this username is taken already !')
+            return render(request, 'register.html')
+    return render(request, 'register.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+def home_view(request):
+    return render(request, 'home.html')
+
+
